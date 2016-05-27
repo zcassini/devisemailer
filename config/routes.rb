06ed3e87ff6e2181924e2bc73facbcec3d91bc7a
Zac Cassini <zcassini@gmail.com>
2016-05-27 12:49:52 +0000

    setup articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :articles
+  
   devise_for :users, :controllers => { registrations: 'registrations' }
+  
+  resources :articles #_url
+  
   root to: 'pages#index'
   get 'pages/contact'
   get 'pages/about'
