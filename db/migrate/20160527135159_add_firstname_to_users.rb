6042836d76df91f3a473242960949c9bd82bae3d
Zac Cassini <zcassini@gmail.com>
2016-05-27 14:03:23 +0000

    remove string column...doh"

--- db/migrate/20160527135159_add_firstname_to_users.rb
+++ db/migrate/20160527135159_add_firstname_to_users.rb
@@ -1,6 +1,5 @@
 class AddFirstnameToUsers < ActiveRecord::Migration
   def change
     add_column :users, :firstname, :string
-    add_column :users, :string, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 20160527135606) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "firstname"
-    t.string   "string"
     t.string   "lastname"
   end
 
