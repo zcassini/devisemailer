dc103e8aac649382218db7a804dfbcc6d359583c
Zac Cassini <zcassini@gmail.com>
2016-05-28 01:04:39 +0000

    fix my db migration git screwup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160527135606) do
+ActiveRecord::Schema.define(version: 20160527132601) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -22,13 +22,13 @@ ActiveRecord::Schema.define(version: 20160527135606) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",               default: "", null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "username",               default: "",    null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -37,8 +37,9 @@ ActiveRecord::Schema.define(version: 20160527135606) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "admin",                  default: false
     t.string   "firstname"
     t.string   "lastname"
   end
