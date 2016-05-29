e32c69765f1d5ec1aeb37913fa420a905b89e55c
Zac Cassini <zcassini@gmail.com>
2016-05-29 04:03:30 +0000

    fixup comments and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,7 @@ class ArticlesController < ApplicationController
   # GET /articles.json
   def index
     @articles = Article.all
+    @recent_articles = Article.last(5)
   end
 
   # GET /articles/1
